c709a247259ec0e3cd7dff2ea703a679d272d8c8
白衣 <ixrjog@qq.com>
2024-02-23 11:26:35 +0800

    fixbug

--- cratos-manage/src/main/java/com/baiyi/cratos/aspect/SetSessionUserToParamAspect.java
+++ cratos-manage/src/main/java/com/baiyi/cratos/aspect/SetSessionUserToParamAspect.java
@@ -1,11 +1,15 @@
 package com.baiyi.cratos.aspect;
 
+import com.baiyi.cratos.annotation.SetSessionUserToParam;
 import com.baiyi.cratos.domain.ISetSessionUser;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
@@ -14,14 +18,17 @@ import java.util.Arrays;
  * @Date 2024/2/23 09:57
  * @Version 1.0
  */
+@Slf4j
+@Aspect
+@Component
 public class SetSessionUserToParamAspect {
 
     @Pointcut(value = "@annotation(com.baiyi.cratos.annotation.SetSessionUserToParam)")
     public void annotationPoint() {
     }
 
-    @Before(value = "@annotation(author)")
-    public void beforeAdvice(JoinPoint joinPoint) {
+    @Before(value = "@annotation(setSessionUserToParam)")
+    public void beforeAdvice(JoinPoint joinPoint, SetSessionUserToParam setSessionUserToParam) {
         Arrays.stream(joinPoint.getArgs())
                 .filter(arg -> arg instanceof ISetSessionUser)
                 .map(arg -> (ISetSessionUser) arg)
