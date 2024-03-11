074c6d951ba702c700d8138ea240e0f0a1b70c18
白衣 <ixrjog@qq.com>
2024-03-11 13:57:39 +0800

    构建脚本

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo $JAVA_HOME
+java -version
+/Users/liangjian/Documents/workspace/maven-3.3.3/bin/mvn -Dmaven.test.skip=true clean package -e -T 2C -P prod -U -am
+
+# upload oss
+ossutil -c ~/.ossutilconfig cp -f -u ./cratos-manage/target/cratos-manage-prod.jar oss://opscloud4-web-hz/package/
