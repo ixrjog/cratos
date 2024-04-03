3504582d5641e9d03dfd0a54c95efac4ca322fdb
白衣 <ixrjog@qq.com>
2024-04-03 15:35:36 +0800

    代码优化

--- cratos-manage/src/main/java/com/baiyi/cratos/facade/TrafficLayerFacade.java
+++ cratos-manage/src/main/java/com/baiyi/cratos/facade/TrafficLayerFacade.java
@@ -12,6 +12,4 @@ public interface TrafficLayerFacade {
 
     TrafficLayerRecordVO.RecordDetails queryRecordDetails(TrafficLayerRecordParam.QueryRecordDetails queryRecordDetails);
 
-    String queryRecordDetailsStringTable(TrafficLayerRecordParam.QueryRecordDetails queryRecordDetails);
-
 }
