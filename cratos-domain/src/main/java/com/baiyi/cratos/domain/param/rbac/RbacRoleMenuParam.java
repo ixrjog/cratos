f3c43b4eb76b6f86609a239ffd66d9ac0d715d2b
修远 <448613222@qq.com>
2024-04-12 16:35:50 +0800

    fix

--- cratos-domain/src/main/java/com/baiyi/cratos/domain/param/rbac/RbacRoleMenuParam.java
+++ cratos-domain/src/main/java/com/baiyi/cratos/domain/param/rbac/RbacRoleMenuParam.java
@@ -3,9 +3,10 @@ package com.baiyi.cratos.domain.param.rbac;
 import com.baiyi.cratos.domain.generator.RbacRoleMenu;
 import com.baiyi.cratos.domain.param.IToTarget;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -44,13 +45,15 @@ public class RbacRoleMenuParam {
     }
 
     @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Schema
     public static class SaveRoleMenu implements IToRoleMenus {
 
-        @NonNull
+        @NotNull
         private Integer roleId;
 
-        @NotEmpty
+        @NotNull
         private List<Integer> menuIds;
 
     }
