d7ed9888fe690bd004a037a2f09631bdf482a681
白衣 <ixrjog@qq.com>
2024-05-28 18:12:13 +0800

    升级Maven版本 3.9.7

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 echo $JAVA_HOME
 java -version
-/Users/liangjian/Documents/workspace/maven-3.3.3/bin/mvn -Dmaven.test.skip=true clean package -e -T 2C -P prod -U -am
+
+# apache-maven-3.9.7
+/Users/liangjian/Documents/workspace/apache-maven-3.9.7/bin/mvn -Dmaven.test.skip=true clean package -e -T 2C -P prod -U -am
+# /Users/liangjian/Documents/workspace/maven-3.3.3/bin/mvn -Dmaven.test.skip=true clean package -e -T 2C -P prod -U -am
 
 # Upload to OSS
 ossutil -c ~/.ossutilconfig cp -f -u ./cratos-manage/target/cratos-manage-prod.jar oss://opscloud4-web-hz/package/
