f0afdc5aa9104f8641bdf733b71f85342c2f3411
白衣 <ixrjog@qq.com>
2024-06-11 10:14:58 +0800

    kubernetes node增加容量索引

--- cratos-eds-kubernetes/src/main/java/com/baiyi/cratos/eds/kubernetes/provider/EdsKubernetesNodeAssetProvider.java
+++ cratos-eds-kubernetes/src/main/java/com/baiyi/cratos/eds/kubernetes/provider/EdsKubernetesNodeAssetProvider.java
@@ -1,5 +1,7 @@
 package com.baiyi.cratos.eds.kubernetes.provider;
 
+import com.baiyi.cratos.domain.generator.EdsAsset;
+import com.baiyi.cratos.domain.generator.EdsAssetIndex;
 import com.baiyi.cratos.eds.core.annotation.EdsInstanceAssetType;
 import com.baiyi.cratos.eds.core.config.EdsKubernetesConfigModel;
 import com.baiyi.cratos.eds.core.enums.EdsAssetTypeEnum;
@@ -15,10 +17,16 @@ import com.baiyi.cratos.eds.kubernetes.repo.KubernetesNodeRepo;
 import com.baiyi.cratos.facade.SimpleEdsFacade;
 import com.baiyi.cratos.service.CredentialService;
 import com.baiyi.cratos.service.EdsAssetService;
+import com.google.common.collect.Lists;
 import io.fabric8.kubernetes.api.model.Node;
+import io.fabric8.kubernetes.api.model.NodeStatus;
+import io.fabric8.kubernetes.api.model.Quantity;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * &#064;Author  baiyi
@@ -52,6 +60,42 @@ public class EdsKubernetesNodeAssetProvider extends BaseEdsKubernetesAssetProvid
     protected List<Node> listEntities(String namespace,
                                       ExternalDataSourceInstance<EdsKubernetesConfigModel.Kubernetes> instance) throws EdsQueryEntitiesException {
         return List.of();
+    }
+
+    /**
+     * 增加容量索引
+     *
+     * @param instance
+     * @param edsAsset
+     * @param entity
+     * @return
+     */
+    @Override
+    protected List<EdsAssetIndex> toEdsAssetIndexList(
+            ExternalDataSourceInstance<EdsKubernetesConfigModel.Kubernetes> instance, EdsAsset edsAsset, Node entity) {
+        Optional<Map<String, Quantity>> optionalMap = Optional.of(entity)
+                .map(Node::getStatus)
+                .map(NodeStatus::getCapacity);
+        if (optionalMap.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<EdsAssetIndex> indices = Lists.newArrayList();
+        Map<String, Quantity> quantityMap = optionalMap.get();
+        if (quantityMap.containsKey("cpu")) {
+            indices.add(toEdsAssetIndex(edsAsset, "status.capacity.cpu", formatQuantity(quantityMap.get("cpu"))));
+        }
+        if (quantityMap.containsKey("ephemeral-storage")) {
+            indices.add(toEdsAssetIndex(edsAsset, "status.capacity.ephemeral-storage",
+                    formatQuantity(quantityMap.get("ephemeral-storage"))));
+        }
+        if (quantityMap.containsKey("memory")) {
+            indices.add(toEdsAssetIndex(edsAsset, "status.capacity.memory", formatQuantity(quantityMap.get("memory"))));
+        }
+        return indices;
+    }
+
+    private String formatQuantity(Quantity quantity) {
+        return quantity.getAmount() + quantity.getFormat();
     }
 
 }
