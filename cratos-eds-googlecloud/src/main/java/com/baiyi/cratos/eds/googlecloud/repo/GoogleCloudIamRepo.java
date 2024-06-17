a2a21c5965c6073c1033db46831c2349f215d644
白衣 <ixrjog@qq.com>
2024-06-17 18:30:18 +0800

    测试

--- cratos-eds-googlecloud/src/main/java/com/baiyi/cratos/eds/googlecloud/repo/GoogleCloudIamRepo.java
+++ cratos-eds-googlecloud/src/main/java/com/baiyi/cratos/eds/googlecloud/repo/GoogleCloudIamRepo.java
@@ -3,8 +3,10 @@ package com.baiyi.cratos.eds.googlecloud.repo;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * &#064;Author  baiyi
@@ -13,13 +15,20 @@ import java.io.IOException;
  */
 public class GoogleCloudIamRepo {
 
-    public static void test() throws IOException {
+
+    private static final String key = """
+            {
+             ...
+            }
+            """;
+
+    public static void test1() throws IOException {
 
         String credentialPath = "/Users/liangjian/cratos-data/palmpay-nigeria-3b0d6496b7e4.json";
 
-      //  InputStream targetStream = IOUtils.toInputStream(credential, StandardCharsets.UTF_8.name());
+        //  InputStream targetStream = IOUtils.toInputStream(credential, StandardCharsets.UTF_8.name());
 
-       // GoogleCredentials credentials = GoogleCredentials.fromStream(targetStream );
+        // GoogleCredentials credentials = GoogleCredentials.fromStream(targetStream );
 
         GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialPath));
 
@@ -30,6 +39,23 @@ public class GoogleCloudIamRepo {
         //AccessToken token = credentials.refreshAccessToken();
 
         System.out.println(token.getTokenValue());
+
+    }
+
+    public static void test2() {
+        try (InputStream is = new ByteArrayInputStream(key.getBytes())) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(is);
+            credentials.refreshIfExpired();
+            AccessToken token = credentials.getAccessToken();
+            // OR
+            //AccessToken token = credentials.refreshAccessToken();
+
+            System.out.println(token.getTokenValue());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
---
+++ cratos-manage/src/test/java/com/baiyi/cratos/common/GCPTest.java
@@ -0,0 +1,19 @@
+package com.baiyi.cratos.common;
+
+import com.baiyi.cratos.BaseUnit;
+import com.baiyi.cratos.eds.googlecloud.repo.GoogleCloudIamRepo;
+import org.junit.jupiter.api.Test;
+
+/**
+ * &#064;Author  baiyi
+ * &#064;Date  2024/6/17 下午4:14
+ * &#064;Version 1.0
+ */
+public class GCPTest extends BaseUnit {
+
+    @Test
+    void test() {
+        GoogleCloudIamRepo.test2();
+    }
+
+}
