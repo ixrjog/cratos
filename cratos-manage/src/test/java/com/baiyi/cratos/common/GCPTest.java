5dc5a2432491953f94d44b472beb1d96d3b80e86
修远 <448613222@qq.com>
2024-07-26 17:59:12 +0800

    daily

--- cratos-manage/src/test/java/com/baiyi/cratos/common/GCPTest.java
+++ cratos-manage/src/test/java/com/baiyi/cratos/common/GCPTest.java
@@ -4,8 +4,11 @@ import com.baiyi.cratos.BaseUnit;
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.certificatemanager.v1.CertificateManagerClient;
+import com.google.cloud.certificatemanager.v1.CertificateManagerSettings;
 import com.google.cloud.iam.admin.v1.IAMClient;
 import com.google.cloud.iam.admin.v1.IAMSettings;
+import com.google.iam.admin.v1.ListRolesRequest;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
@@ -22,15 +25,33 @@ public class GCPTest extends BaseUnit {
 
     @Test
     void test1() throws IOException {
-
         String credentialPath = "/Users/zl/cratos-data/key.json";
         GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream(credentialPath));
         IAMSettings settings = IAMSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
         try (IAMClient client = IAMClient.create(settings)) {
-            IAMClient.ListServiceAccountsPagedResponse response = client.listServiceAccounts(PROJECT_NAME);
-            System.out.println(response);
+            IAMClient.ListServiceAccountsPagedResponse listServiceAccountsPagedResponse = client.listServiceAccounts(PROJECT_NAME);
+            System.out.println(listServiceAccountsPagedResponse);
+
+            ListRolesRequest listRolesRequest = ListRolesRequest.newBuilder().setParent(PROJECT_NAME).build();
+            IAMClient.ListRolesPagedResponse listRolesResponse = client.listRoles(listRolesRequest);
+            System.out.println(listRolesResponse);
+        }
+    }
+
+
+
+    @Test
+    void certificateTest1() throws IOException {
+        String credentialPath = "/Users/zl/cratos-data/key.json";
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream(credentialPath));
+        CertificateManagerSettings settings = CertificateManagerSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                .build();
+        try (CertificateManagerClient client = CertificateManagerClient.create(settings)) {
+           CertificateManagerClient.ListCertificatesPagedResponse listCertificatesPagedResponse=  client.listCertificates(PROJECT_NAME);
+            System.out.println(listCertificatesPagedResponse);
         }
     }
 
