de850a51fa78e7ba8ccbb75b84401090e60378fc
修远 <448613222@qq.com>
2024-07-29 16:53:26 +0800

    commit

--- cratos-manage/src/test/java/com/baiyi/cratos/common/GCPTest.java
+++ cratos-manage/src/test/java/com/baiyi/cratos/common/GCPTest.java
@@ -9,11 +9,21 @@ import com.google.cloud.certificatemanager.v1.CertificateManagerSettings;
 import com.google.cloud.certificatemanager.v1.LocationName;
 import com.google.cloud.iam.admin.v1.IAMClient;
 import com.google.cloud.iam.admin.v1.IAMSettings;
-import com.google.iam.admin.v1.ListRolesRequest;
+import com.google.cloud.resourcemanager.v3.ProjectName;
+import com.google.cloud.resourcemanager.v3.ProjectsClient;
+import com.google.cloud.resourcemanager.v3.ProjectsSettings;
+import com.google.iam.v1.Binding;
+import com.google.iam.v1.GetIamPolicyRequest;
+import com.google.iam.v1.GetPolicyOptions;
+import com.google.iam.v1.Policy;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * &#064;Author  baiyi
@@ -33,26 +43,65 @@ public class GCPTest extends BaseUnit {
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
         try (IAMClient client = IAMClient.create(settings)) {
-            IAMClient.ListServiceAccountsPagedResponse listServiceAccountsPagedResponse = client.listServiceAccounts(PROJECT_NAME);
-            System.out.println(listServiceAccountsPagedResponse);
+//            ListServiceAccountsRequest request = ListServiceAccountsRequest.newBuilder()
+//                    .setName(PROJECT_NAME)
+//                    .setPageSize(100)
+//                    .build();
+//            IAMClient.ListServiceAccountsPagedResponse listServiceAccountsPagedResponse = client.listServiceAccounts(
+//                    request);
+//            System.out.println(listServiceAccountsPagedResponse);
+//            GetRoleRequest getRoleRequest = GetRoleRequest.newBuilder().setName("roles/viewer").build();
+//            Role role = client.getRole(getRoleRequest);
+//            role.getIncludedPermissionsList().forEach(permission -> System.out.println(permission));
 
-            ListRolesRequest listRolesRequest = ListRolesRequest.newBuilder().setParent(PROJECT_NAME).build();
-            IAMClient.ListRolesPagedResponse listRolesResponse = client.listRoles(listRolesRequest);
-            System.out.println(listRolesResponse);
+
+//            ListRolesRequest listRolesRequest = ListRolesRequest.newBuilder().setParent(PROJECT_NAME).build();
+//            IAMClient.ListRolesPagedResponse listRolesResponse = client.listRoles(listRolesRequest);
+//            System.out.println(listRolesResponse);
         }
     }
 
     @Test
-    void certificateTest1() throws IOException {
+    void certificateTest() throws IOException {
         String credentialPath = "/Users/zl/cratos-data/key.json";
         GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream(credentialPath));
         CertificateManagerSettings settings = CertificateManagerSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
         try (CertificateManagerClient client = CertificateManagerClient.create(settings)) {
-            LocationName parent = LocationName.of("palmpay-nigeria", "global");
-            CertificateManagerClient.ListCertificatesPagedResponse listCertificatesPagedResponse=  client.listCertificates(parent);
+            LocationName parent = LocationName.of(PROJECT_ID, "global");
+            CertificateManagerClient.ListCertificatesPagedResponse listCertificatesPagedResponse = client.listCertificates(
+                    parent);
             System.out.println(listCertificatesPagedResponse);
+        }
+    }
+
+
+    @Test
+    void projectTest() throws IOException {
+        String credentialPath = "/Users/zl/cratos-data/key.json";
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(new FileInputStream(credentialPath));
+        ProjectsSettings settings = ProjectsSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                .build();
+        try (ProjectsClient client = ProjectsClient.create(settings)) {
+            GetPolicyOptions options = GetPolicyOptions.newBuilder()
+                    .build();
+            GetIamPolicyRequest request = GetIamPolicyRequest.newBuilder()
+                    .setResource(ProjectName.of(PROJECT_ID).toString())
+                    .build();
+            Policy policy = client.getIamPolicy(request);
+
+            Map<String, List<String>> memberRolesMap = new HashMap<>();
+
+            // Populate the map with members and roles from the policy bindings
+            for (Binding binding : policy.getBindingsList()) {
+                String role = binding.getRole();
+                for (String member : binding.getMembersList()) {
+                    memberRolesMap.computeIfAbsent(member, k -> new ArrayList<>()).add(role);
+                }
+            }
+            System.err.println(memberRolesMap);
         }
     }
 
