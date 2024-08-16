75156de5c8c5f5b294f7356bb829223c97cbfcac
白衣 <ixrjog@qq.com>
2024-08-16 11:12:23 +0800

    daily

--- cratos-manage/src/main/java/com/baiyi/cratos/facade/traffic/model/IngressDetailsModel.java
+++ cratos-manage/src/main/java/com/baiyi/cratos/facade/traffic/model/IngressDetailsModel.java
@@ -6,7 +6,6 @@ import com.google.api.client.util.Lists;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
-import java.util.Collections;
 import java.util.List;
 
 import static com.baiyi.cratos.facade.traffic.TrafficLayerIngressFacadeImpl.INGRESS_TABLE_FIELD_NAME;
@@ -28,6 +27,7 @@ public class IngressDetailsModel {
         private String rule;
         private String service;
         private String lb;
+
         @Override
         public int compareTo(@NonNull IngressEntry o) {
             return service.compareTo(o.getService());
@@ -39,16 +39,21 @@ public class IngressDetailsModel {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class IngressEntries {
+        @Builder.Default
         private List<IngressEntry> entries = Lists.newArrayList();
+
         public void add(IngressEntry ingressEntry) {
             entries.add(ingressEntry);
         }
+
         @Override
         public String toString() {
             PrettyTable ingressTable = PrettyTable.fieldNames(INGRESS_TABLE_FIELD_NAME);
-            Collections.sort(entries);
-            entries.forEach(ingressEntry -> ingressTable.addRow(ingressEntry.getKubernetes(), ingressEntry.getIngress(),
-                    ingressEntry.getRule(), ingressEntry.getService(), ingressEntry.getLb()));
+            entries.stream()
+                    .sorted()
+                    .forEach(
+                            ingressEntry -> ingressTable.addRow(ingressEntry.getKubernetes(), ingressEntry.getIngress(),
+                                    ingressEntry.getRule(), ingressEntry.getService(), ingressEntry.getLb()));
             return ingressTable.toString();
         }
     }
