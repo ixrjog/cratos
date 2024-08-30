943092544d30941822c18e7dcb8d50006dfff790
白衣 <ixrjog@qq.com>
2024-08-30 18:33:00 +0800

    daily

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ java -version
 # apache-maven-3.9.7
 ~/Documents/workspace/apache-maven-3.9.7/bin/mvn -Dmaven.test.skip=true clean package -e -T 2C -P prod -U -am
 
+md5 ./cratos-manage/target/cratos-manage-prod.jar
 # Upload to OSS
 ossutil -c ~/.ossutilconfig cp -f -u ./cratos-manage/target/cratos-manage-prod.jar oss://opscloud4-web-hz/package/
