937d3680b385671b3b30becae0c951736ce786be
白衣 <ixrjog@qq.com>
2024-11-27 10:29:10 +0800

    优化代码

--- cratos-manage/src/main/java/com/baiyi/cratos/wrapper/EdsInstanceWrapper.java
+++ cratos-manage/src/main/java/com/baiyi/cratos/wrapper/EdsInstanceWrapper.java
@@ -1,6 +1,7 @@
 package com.baiyi.cratos.wrapper;
 
 import com.baiyi.cratos.annotation.BusinessWrapper;
+import com.baiyi.cratos.common.configuration.CachingConfiguration;
 import com.baiyi.cratos.domain.enums.BusinessTypeEnum;
 import com.baiyi.cratos.domain.generator.EdsInstance;
 import com.baiyi.cratos.domain.view.eds.EdsAssetTypeVO;
@@ -14,7 +15,10 @@ import com.baiyi.cratos.wrapper.base.BaseDataTableConverter;
 import com.baiyi.cratos.wrapper.base.IBaseWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Comparator;
 import java.util.List;
@@ -47,10 +51,19 @@ public class EdsInstanceWrapper extends BaseDataTableConverter<EdsInstanceVO.Eds
                 .toList();
         vo.setInstanceAssetTypes(instanceAssetTypes);
         // Version
+        String version = ((EdsInstanceWrapper) AopContext.currentProxy()).queryVersion(vo.getId());
+        if (StringUtils.hasText(version)) {
+            vo.setVersion(version);
+        }
+    }
+
+    @Cacheable(cacheNames = CachingConfiguration.RepositoryName.SHORT_TERM, key = "'EDS:INSTANCE:VERSION:ID:' + #instanceId", unless = "#result == null")
+    public String queryVersion(int instanceId) {
         try {
-            EdsInstanceVersionProviderHolder<?> holder = versionHolderBuilder.newHolder(vo.getId());
-            vo.setVersion(holder.version());
+            EdsInstanceVersionProviderHolder<?> holder = versionHolderBuilder.newHolder(instanceId);
+            return holder.version();
         } catch (EdsInstanceVersionProviderException ignored) {
+            return "";
         }
     }
 
