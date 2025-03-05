2562d1fa276e4a8089e900b89384a101f393f475
白衣 <ixrjog@qq.com>
2025-03-05 10:16:44 +0800

    模块重命名

---
+++ cratos-manage/src/main/java/com/baiyi/cratos/facade/identity/extension/cloud/impl/HwcIdentityProvider.java
@@ -0,0 +1,9 @@
+package com.baiyi.cratos.facade.identity.extension.cloud.impl;
+
+/**
+ * &#064;Author  baiyi
+ * &#064;Date  2025/3/5 09:48
+ * &#064;Version 1.0
+ */
+public class HwcIdentityProvider {
+}
